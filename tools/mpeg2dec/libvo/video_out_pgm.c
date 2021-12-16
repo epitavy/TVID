909b8b9047aaac0fd48cabcf6cf4a6091a50bd20
Eliaz Pitavy <eliaz.pitavy@epita.fr>
2021-12-16 21:49:36 +0100

    Add `rff`, `tff` and `prog` flags in pgm output
    Signed-off-by: Eliaz Pitavy <eliaz.pitavy@epita.fr>

--- tools/mpeg2dec/libvo/video_out_pgm.c
+++ tools/mpeg2dec/libvo/video_out_pgm.c
@@ -117,8 +117,8 @@ static int pgm_setup (vo_instance_t * _instance, unsigned int width,
     instance->height = height;
     instance->chroma_width = chroma_width;
     instance->chroma_height = chroma_height;
-    sprintf (instance->header, "P5\n%d %d\n255\n#ips:%d\n", 2 * chroma_width,
-	     height + chroma_height, cadence_ips);
+    sprintf (instance->header, "P5\n%d %d\n255\n#ips:%d rff:%d tff:%d prog:%d\n", 2 * chroma_width,
+	     height + chroma_height, cadence_ips, is_rff, is_tff, is_prog);
     result->convert = NULL;
     return 0;
 }
